2e636d588f6dba15e58fe20a4b281bf7bffee760
Baptiste Langlade <baptouuuu@gmail.com>
2016-01-14 08:31:50 +0100

    add class to inject data into an object

---
+++ ReflectionObject.php
@@ -0,0 +1,140 @@
+<?php
+declare(strict_types=1);
+
+namespace Innmind\Reflection;
+
+use Innmind\Reflection\Exception\InvalidArgumentException;
+use Innmind\Reflection\Exception\LogicException;
+use Innmind\Immutable\Collection;
+use Innmind\Immutable\CollectionInterface;
+use Innmind\Immutable\StringPrimitive;
+
+class ReflectionObject
+{
+    private $object;
+    private $properties;
+    private $setter;
+
+    public function __construct(
+        $object,
+        CollectionInterface $properties = null
+    ) {
+        if (!is_object($object)) {
+            throw new InvalidArgumentException;
+        }
+
+        $this->object = $object;
+
+        if ($properties === null) {
+            $properties = new Collection([]);
+        }
+
+        $this->properties = $properties;
+        $this->setter = new StringPrimitive('set%s');
+    }
+
+    /**
+     * Add a property that will be injected
+     *
+     * @param string $name
+     * @param mixed $value
+     *
+     * @return self
+     */
+    public function withProperty(string $name, $value)
+    {
+        return new self(
+            $this->object,
+            $this->properties->set($name, $value)
+        );
+    }
+
+    /**
+     * Return the collection of properties that will be injected in the object
+     *
+     * @return CollectionInterface
+     */
+    public function getProperties() : CollectionInterface
+    {
+        return $this->properties;
+    }
+
+    /**
+     * Return the object with the list of properties set on it
+     *
+     * @return object
+     */
+    public function buildObject()
+    {
+        foreach ($this->properties as $key => $value) {
+            $this->inject($key, $value);
+        }
+
+        return $this->object;
+    }
+
+    /**
+     * Inject the given key/value pair into the object
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return void
+     */
+    private function inject(string $key, $value)
+    {
+        $refl = new \ReflectionObject($this->object);
+
+        $setter = $this->setter->sprintf(ucfirst($key));
+
+        if ($refl->hasMethod((string) $setter)) {
+            $this->injectBySetter((string) $setter, $value);
+        } else if ($refl->hasMethod($key)) {
+            $this->injectBySetter($key, $value);
+        } else if ($refl->hasProperty($key)) {
+            $this->injectByReflection(
+                $refl->getProperty($key),
+                $value
+            );
+        } else {
+            throw new LogicException(sprintf(
+                'Property "%s" not found',
+                $key
+            ));
+        }
+    }
+
+    /**
+     * Inject the value through the given setter
+     *
+     * @param string $setter
+     * @param mixed $value
+     *
+     * @return void
+     */
+    private function injectBySetter(string $setter, $value)
+    {
+        $this->object->$setter($value);
+    }
+
+    /**
+     * Inject the given value through reflection
+     *
+     * @param ReflectionProperty $refl
+     * @param mixed $value
+     *
+     * @return void
+     */
+    public function injectByReflection(\ReflectionProperty $refl, $value)
+    {
+        if (!$refl->isPublic()) {
+            $refl->setAccessible(true);
+        }
+
+        $refl->setValue($this->object, $value);
+
+        if (!$refl->isPublic()) {
+            $refl->setAccessible(false);
+        }
+    }
+}
---
+++ Tests/ReflectionObjectTest.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+namespace Innmind\Reflection\Tests;
+
+use Innmind\Reflection\ReflectionObject;
+
+class ReflectionObjectTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBuildWithoutProperties()
+    {
+        $o = new \stdClass;
+        $refl = new ReflectionObject($o);
+
+        $o2 = $refl->buildObject();
+
+        $this->assertSame($o, $o2);
+        $this->assertSame([], $refl->getProperties()->toPrimitive());
+    }
+
+    /**
+     * @expectedException Innmind\Reflection\Exception\InvalidArgumentException
+     */
+    public function testThrowWhenNotUsingAnObject()
+    {
+        new ReflectionObject([]);
+    }
+
+    public function testAddPropertyToInject()
+    {
+        $o = new \stdClass;
+        $refl = new ReflectionObject($o);
+        $refl2 = $refl->withProperty('foo', 'bar');
+
+        $this->assertInstanceOf(ReflectionObject::class, $refl2);
+        $this->assertNotSame($refl, $refl2);
+        $this->assertSame([], $refl->getProperties()->toPrimitive());
+        $this->assertSame(
+            ['foo' => 'bar'],
+            $refl2->getProperties()->toPrimitive()
+        );
+    }
+
+    public function testBuild()
+    {
+        $o = new class() {
+            private $a;
+            protected $b;
+            private $c;
+            private $d;
+
+            public function setC($value)
+            {
+                $this->c = $value;
+            }
+
+            public function d($value)
+            {
+                $this->d = $value;
+            }
+
+            public function dump()
+            {
+                return [$this->a, $this->b, $this->c, $this->d];
+            }
+        };
+
+        $this->assertSame([null, null, null, null], $o->dump());
+
+        (new ReflectionObject($o))
+            ->withProperty('a', 1)
+            ->withProperty('b', 2)
+            ->withProperty('c', 3)
+            ->withProperty('d', 4)
+            ->buildObject();
+
+        $this->assertSame([1, 2, 3, 4], $o->dump());
+    }
+
+    /**
+     * @expectedException Innmind\Reflection\Exception\LogicException
+     * @expectedExceptionMessage Property "a" not found
+     */
+    public function testThrowWhenPropertyNotFound()
+    {
+        (new ReflectionObject(new \stdClass))
+            ->withProperty('a', 1)
+            ->buildObject();
+    }
+}
