2ebc1025b7f9635a524fde230fa65a35da8c0bc5
Baptiste Langlade <baptouuuu@gmail.com>
2017-01-18 07:59:36 +0100

    add constructor less instanciator

---
+++ tests/Instanciator/ConstructorLessInstanciatorTest.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Reflection\Instanciator;
+
+use Innmind\Reflection\{
+    InstanciatorInterface,
+    Instanciator\ConstructorLessInstanciator
+};
+use Innmind\Immutable\Collection;
+
+class ConstructorLessInstanciatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            InstanciatorInterface::class,
+            new ConstructorLessInstanciator
+        );
+    }
+
+    public function testBuild()
+    {
+        $instanciator = new ConstructorLessInstanciator;
+        $object = new class('bar') {
+            public $foo;
+
+            public function __construct(string $foo)
+            {
+                $this->foo = $foo;
+            }
+        };
+
+        $this->assertInstanceOf(
+            'stdClass',
+            $instanciator->build(
+                'stdClass',
+                new Collection([])
+            )
+        );
+
+        $builtObject = $instanciator->build(
+            get_class($object),
+            new Collection([])
+        );
+
+        $this->assertInstanceOf(get_class($object), $builtObject);
+        $this->assertNull($builtObject->foo);
+    }
+
+    public function testGetParameters()
+    {
+        $instanciator = new ConstructorLessInstanciator;
+        $object = new class('bar') {
+            public function __construct(string $foo)
+            {
+            }
+        };
+
+        $this->assertSame(
+            [],
+            $instanciator->getParameters('stdClass')->toPrimitive()
+        );
+        $this->assertSame(
+            [],
+            $instanciator->getParameters(get_class($object))->toPrimitive()
+        );
+    }
+}
