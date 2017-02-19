fd135f491b23ee4a7d288b4b882e25ad61070733
Baptiste Langlade <baptouuuu@gmail.com>
2017-02-19 16:15:59 +0100

    add injection delegation strategy

---
+++ src/InjectionStrategy/DelegationStrategy.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Reflection\InjectionStrategy;
+
+use Innmind\Reflection\{
+    InjectionStrategyInterface,
+    Exception\InvalidArgumentException,
+    Exception\PropertyNotFoundException
+};
+use Innmind\Immutable\{
+    StreamInterface,
+    Map
+};
+
+final class DelegationStrategy implements InjectionStrategyInterface
+{
+    private $strategies;
+    private $cache;
+
+    /**
+     * @param StreamInterface<InjectionStrategyInterface> $strategies
+     */
+    public function __construct(StreamInterface $strategies)
+    {
+        if ((string) $strategies->type() !== InjectionStrategyInterface::class) {
+            throw new InvalidArgumentException;
+        }
+
+        $this->strategies = $strategies;
+        $this->cache = new Map('string', InjectionStrategyInterface::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($object, string $property, $value): bool
+    {
+        return $this
+            ->strategies
+            ->reduce(
+                false,
+                function(bool $supports, InjectionStrategyInterface $strategy) use ($object, $property, $value): bool {
+                    if ($supports === true) {
+                        return true;
+                    }
+
+                    return $strategy->supports($object, $property, $value);
+                }
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function inject($object, string $property, $value)
+    {
+        $key = $this->generateKey($object, $property);
+
+        if ($this->cache->contains($key)) {
+            return $this
+                ->cache
+                ->get($key)
+                ->inject($object, $property, $value);
+        }
+
+        $strategy = $this->strategies->reduce(
+            null,
+            function($target, InjectionStrategyInterface $strategy) use ($object, $property, $value) {
+                if ($target instanceof InjectionStrategyInterface) {
+                    return $target;
+                }
+
+                if ($strategy->supports($object, $property, $value)) {
+                    return $strategy;
+                }
+            }
+        );
+
+        if (!$strategy instanceof InjectionStrategyInterface) {
+            throw new PropertyNotFoundException;
+        }
+
+        $this->cache = $this->cache->put($key, $strategy);
+
+        return $strategy->inject($object, $property, $value);
+    }
+
+    private function generateKey($object, string $property): string
+    {
+        return get_class($object).'::'.$property;
+    }
+}
